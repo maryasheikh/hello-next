77acaa4e14f6fa755d6967bc461def4d6a92d36b
maryasheikh <leslierose645@gmail.com>
2024-11-24 23:07:14 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default function HelloWorld() {
   return (
     <div>
-    <h1 className= "font-bold underline flex justify-center text-center items-center text-pink-600 h-screen bg-blue-200 text-4xl font-serif ">Hello World!</h1>
-    </div>
-    
+    <h1 className= "font-bold underline flex justify-center text-center items-center text-pink-600 h-screen bg-blue-200 text-4xl font-serif">Hello World!</h1>
+    </div>  
   );
 }
